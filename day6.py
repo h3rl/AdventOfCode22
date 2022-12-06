e319cb40bab746ab1a5199ed0e3a0e502edb86c9
h3rl <halvard.erla@gmail.com>
2022-12-06 09:55:07 +0100

    move isUnique

--- day6.py
+++ day6.py
@@ -1,22 +1,18 @@
 import sys
 import pathlib
 sys.path.append(pathlib.Path().resolve().__str__())
-from util import parseAOC, parseAOC_test
-
-def allUnique(x):
-    seen = set()
-    return not any(i in seen or seen.add(i) for i in x)
+from util import parseAOC, parseAOC_test, isUnique
 
 def part_a(data):
     for i in range(4, len(data)):
         d = data[i-4:i]
-        if allUnique(d):
+        if isUnique(d):
             return i
 
 def part_b(data):
     for i in range(14, len(data)):
         d = data[i-14:i]
-        if allUnique(d):
+        if isUnique(d):
             return i
 
 def main():
